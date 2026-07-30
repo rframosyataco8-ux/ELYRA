af90a9a0549a344156af86ad87f028ec02ae86a4
Fabricio_Ramos <rframosyataco8@gmail.com>
2026-07-30 10:59:28 -0500

    agent: enrutar find_files/collect_files/copy_file/mkdir a hooks

---
+++ electron/agent-tool-gate.cjs
@@ -0,0 +1,16 @@
+/** Tools que van a agent-hooks (Python + FS skills nativos) */
+module.exports = new Set([
+  'scan_folder',
+  'analyze_excel',
+  'summarize_pdf',
+  'read_docx',
+  'write_docx',
+  'write_pptx',
+  'html_dashboard',
+  'find_files',
+  'collect_files',
+  'copy_file',
+  'mkdir',
+  'remember',
+  'recall',
+]);
