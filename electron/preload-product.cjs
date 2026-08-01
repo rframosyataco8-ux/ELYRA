248e813c14e8b9ea6921ef894d673beaafc6bfc5
Fabricio_Ramos <rframosyataco8@gmail.com>
2026-08-01 13:10:09 -0500

    feat: minimize/maximize en ventana producto (estilo macOS)

--- electron/preload-product.cjs
+++ electron/preload-product.cjs
@@ -2,4 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('elyraProduct', {
   close: () => ipcRenderer.invoke('close-product-window'),
+  minimize: () => ipcRenderer.invoke('product-window-minimize'),
+  maximize: () => ipcRenderer.invoke('product-window-maximize'),
 });
